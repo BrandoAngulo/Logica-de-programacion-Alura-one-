2122411ea35235c5ef68457ad1ed61785b46af24
BrandoAngulo <88590964+BrandoAngulo@users.noreply.github.com>
2022-02-28 23:40:51 -0500

    aprendiendo a usar selectores en js

---
+++ JavaScript-Parte1-Lenguaje-de-la-web-master/1746-JavaScript-parte1-aula0/js/principal.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector(".titulo");
+			console.log(titulo);
+			console.log(titulo.textContent);
+			//la funcion textContent muestra el contenido que hay en un objeto y tambien lo puede modificar por ejemplo;
+			titulo.textContent = "sabrosa la vida";
+			console.log(titulo.textContent);
