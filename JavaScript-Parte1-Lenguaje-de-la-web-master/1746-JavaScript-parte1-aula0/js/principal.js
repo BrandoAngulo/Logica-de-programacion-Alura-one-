63dfa937b8bbea984c6b52b6d34c1e7e4f99b0f9
BrandoAngulo <88590964+BrandoAngulo@users.noreply.github.com>
2022-03-09 23:52:11 -0500

    operadores logicos, condicionales aprendiendo a usar booleanos

--- JavaScript-Parte1-Lenguaje-de-la-web-master/1746-JavaScript-parte1-aula0/js/principal.js
+++ JavaScript-Parte1-Lenguaje-de-la-web-master/1746-JavaScript-parte1-aula0/js/principal.js
@@ -5,29 +5,45 @@
 			titulo.textContent = "sabrosa la vida";
 			console.log(titulo.textContent); */
 
-			//acceder de javascript a html pero a un id
-			let paciente = document.querySelector("#paciente1");
-			let infoNombre = paciente.querySelector(".info-nombre");
-			let infoPeso = paciente.querySelector(".info-peso");
-			let infoAltura = paciente.querySelector(".info-altura");
-			let infoImc = paciente.querySelector(".info-imc");
+//acceder de javascript a html pero a un id
+let paciente = document.querySelector("#paciente1");
+let infoNombre = paciente.querySelector(".info-nombre");
+let infoPeso = paciente.querySelector(".info-peso");
+let infoAltura = paciente.querySelector(".info-altura");
+let infoImc = paciente.querySelector(".info-imc");
+let pesoCorrecto = true;
+let alturaCorrecta = true;
 
-			let general = infoPeso.textContent;
-			let peso = infoPeso.textContent;
-			let altura = infoAltura.textContent;
+let peso = infoPeso.textContent;
+let altura = infoAltura.textContent;
 
 
-			
-			if((peso < 0) && (peso > 595)){
-				alert("Digita un peso mayor a 0 y menor a 595 KL");
-			}
-			if ((altura < 0) && (altura > 2.51)) {
-				alert("Digita una altura mayor a 0 y menor a 5,51 pies");
-			}
-			
-			//formula del imc peso/altura^2
-			let imc = peso / Math.pow(altura,2); //100 / 2 * 2 = 25 
-			infoImc.textContent = imc;
 
-			console.log(general);
-			console.log(infoImc);
+if ((peso < 0) || (peso > 595)) {
+	console.log("Digita un peso correcto");
+	//modifica el contenido de la clase y le muestro al usuario que esta incorrecto algo
+	infoImc.textContent = "Peso incorrecto";
+	pesoCorrecto = false;
+	
+}
+
+if ((altura < 0) || (altura > 2.51)) {
+	console.log("Digita una altura correcta");
+	//modifica el contenido de la clase y le muestro al usuario que esta incorrecto algo
+	infoImc.textContent = "Altura incorrecta";
+	alturaCorrecta = false;
+}	
+
+	console.log(alturaCorrecta+" "+pesoCorrecto);
+
+	if ((pesoCorrecto === false) && (alturaCorrecta === false)) {
+		
+		infoImc.textContent = "Peso y altura incorrectos";
+	} 
+
+
+if ((pesoCorrecto === true) && (alturaCorrecta === true)) {
+	//formula del imc peso/altura^2
+	let imc = peso / Math.pow(altura, 2); //100 / 2 * 2 = 25 
+	infoImc.textContent = imc;
+} 
