44c363604e9f16a238b04472b3698a8d348d0acf
BrandoAngulo <steven.sm.1992@gmail.com>
2022-04-01 23:28:51 -0500

    finalizando cracion de clases y objetos y haciendo performance de codigo con funciones

--- JavaScript-Parte2-Lenguaje-de-la-web-master/clasesYobjetos/js/form.js
+++ JavaScript-Parte2-Lenguaje-de-la-web-master/clasesYobjetos/js/form.js
@@ -6,42 +6,41 @@ botonAgregar.addEventListener("click", function (event) {
     //ponemos el event dentro de la function anonima como un parametro para que quitemos el actualizado automatico que viene por defecto
     //y llamamos al evento como defaultPrevented();
     event.preventDefault();
+
     //creamos la variable para capturar atraves del querySelector nuestro formulario
     let formulario = document.querySelector('#form-agregar');
     // de esta forma guardamos en cada variable lo que digitamos en el atributo del objeto formulario de HTML
     //con VALUE accedemos a lo que estamos ingresando dentro de cada input y lo estamos guardando en las variables 
     let paciente = capturarPaciente(formulario);
-
+    let pacienteTr = construirTr(paciente);
     //creamos variable para acceder al id de la tabla para guardar nuestros datos nuevos a la tabla
     let tabla = document.querySelector('#tabla-pacientes')
-
-    //creando etiquetas  <tr>fila y columna<td> para agregar a la tabla la tabla 
-    let pacienteTr = document.createElement('tr');
-    let nombreTd = document.createElement('td');
-    let pesoTd = document.createElement('td');
-    let alturaTd = document.createElement('td');
-    let gorduraTd = document.createElement('td');
-    let imcTd = document.createElement('td');
-
-    //accedemos al contenido de cada input usando el objeto paciente para asociarlo a cada etiqueta
-    nombreTd.textContent = paciente.nombre
-    pesoTd.textContent = paciente.peso;
-    alturaTd.textContent = paciente.altura;
-    gorduraTd.textContent = paciente.gordura;
-    //accesamos a la funcion creada del calculaImc para accesar a ella y pasamos los parametros ingresados de peso y altura
-    imcTd.textContent = paciente.imc;
-
-    //Asociando nuestra etiquetas creadas para los nuevos pacientes que se van a agregar a tabla
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
     //designamos a la tabla el dato nuevo del paciente
     tabla.appendChild(pacienteTr);
-
-    //console.log(pacienteTr);
+    //resetear valores y dejar en blanco los input cada ves que enviemos a la tabla los datos, es igual q un value="";
+    formulario.reset();
 });
+
+//se crea una funcion para el tr y los tds
+function construirTr(paciente) {
+    
+     //creando etiquetas  <tr>fila y columna<td> para agregar a la tabla la tabla 
+     let pacienteTr = document.createElement('tr');
+     //creando clase de un tr para HTML
+     pacienteTr.classList.add("paciente");
+
+     //se inserta la funcion dentro del appenChild y como parametros se les pasa el dato del objeto paciente, y la clase HTML
+     //appenchild lo que hace es llamar e insertar el nodo creado osea la lista o tabla etc. va de la mano con createElement y textContent
+     pacienteTr.appendChild(constriurTd(paciente.nombre, "info-nombre"));
+     pacienteTr.appendChild(constriurTd(paciente.peso, "info-peso"));
+     pacienteTr.appendChild(constriurTd(paciente.altura, "info-altura"));
+     pacienteTr.appendChild(constriurTd(paciente.gordura, "info-gordura"));
+     pacienteTr.appendChild(constriurTd(paciente.imc, "info-imc"));
+
+
+     return pacienteTr;
+     
+}
 
 //se crea una funcion como performance del codigo para el ingreso de cada paciente
 function capturarPaciente(formulario) {
@@ -55,4 +54,14 @@ function capturarPaciente(formulario) {
         imc: calcularImc(formulario.peso.value, formulario.altura.value) 
     }
     return paciente;
+}
+
+//funcion para pasar el dato del paciente y crear la clase
+function constriurTd(dato,clase) {
+    let td =  document.createElement('td');
+    //clase creada para agregarla generica del html
+    td.classList.add(clase);
+    //dato contenido por el input q se va enviar a la tabla
+    td.textContent = dato;
+    return td;
 }
---
+++ JavaScript-Parte2-Lenguaje-de-la-web-master/clasesYobjetos/question1/creandoClaseObjeto.js
@@ -0,0 +1,17 @@
+
+// se crea el objeto y sus propiedades
+    let celular = {
+        color: color = "black",
+        marca: marca= "xiomi",
+        modelo: modelo= "9080",
+        anio: anio= "2022",
+        memoria: memoria= "168",
+        peso: peso= "50gr",
+        sistemaOperativo: sistemaOperativo = "12"
+    }
+
+//accedo a cualquier propiedad del objeto 
+console.log(celular.anio);
+
+
+
