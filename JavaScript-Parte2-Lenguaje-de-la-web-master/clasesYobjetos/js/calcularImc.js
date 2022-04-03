32ceee201aac5c50e923ea14196e848d71810a04
BrandoAngulo <steven.sm.1992@gmail.com>
2022-04-03 12:55:31 -0500

    peso y altura validados con funciones

--- JavaScript-Parte2-Lenguaje-de-la-web-master/clasesYobjetos/js/calcularImc.js
+++ JavaScript-Parte2-Lenguaje-de-la-web-master/clasesYobjetos/js/calcularImc.js
@@ -17,14 +17,15 @@ for (let i = 0; i < pacientes.length; i++) {
 	let infoPeso = paciente.querySelector(".info-peso");
 	let infoAltura = paciente.querySelector(".info-altura");
 	let infoImc = paciente.querySelector(".info-imc");
-	let pesoCorrecto = true;
-	let alturaCorrecta = true;
 
 	let peso = infoPeso.textContent;
 	let altura = infoAltura.textContent;
 
+	let pesoCorrecto = validarPeso(peso);
+	let alturaCorrecta = validarAltura(altura);
 
-	if ((peso < 0) || (peso > 595)) {
+
+	if (!pesoCorrecto) {
 		//console.log("Digita un peso correcto");
 		//modifica el contenido de la clase y le muestro al usuario que esta incorrecto algo
 		infoImc.textContent = "Peso incorrecto";
@@ -34,7 +35,7 @@ for (let i = 0; i < pacientes.length; i++) {
 		paciente.classList.add("paciente-incorrecto");
 	}
 
-	if ((altura < 0) || (altura > 2.51)) {
+	if (!alturaCorrecta) {
 		//console.log("Digita una altura correcta");
 		//modifica el contenido de la clase y le muestro al usuario que esta incorrecto algo
 		infoImc.textContent = "Altura incorrecta";
@@ -64,4 +65,22 @@ function calcularImc(peso,altura) {
 
 	//retorno un valor
 	return imc.toFixed(); //tofixed decide poner el numero de digitos decimalesdespues del punto
+}
+
+function validarPeso(peso) {
+	if (peso >= 0 && peso < 595){
+		return true;
+	}else{
+		return false;
+	}
+	
+}
+
+function validarAltura(altura) {
+	if (altura >= 0 && altura < 2.51){
+		return true;
+	}else{
+		return false;
+	}
+	
 }
