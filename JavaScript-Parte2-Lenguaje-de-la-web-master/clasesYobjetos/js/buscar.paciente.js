de27981c7bd09fc1fb9bff42d3c3bf120afb9adf
BrandoAngulo <steven.sm.1992@gmail.com>
2022-06-16 07:37:49 -0500

    condicional para errores en ajax

--- JavaScript-Parte2-Lenguaje-de-la-web-master/clasesYobjetos/js/buscar.paciente.js
+++ JavaScript-Parte2-Lenguaje-de-la-web-master/clasesYobjetos/js/buscar.paciente.js
@@ -1,17 +1,24 @@
 let botonBucar = document.querySelector("#buscar-paciente");
-
 botonBucar.addEventListener("click", function () {
     let xhr = new XMLHttpRequest;
     xhr.open("GET", "https://alura-es-cursos.github.io/api-pacientes/pacientes.json");
     xhr.addEventListener("load", function () {
-        let respuesta = xhr.responseText;
-        //JON.parsear = convertir a valor json
-        let pacientes = JSON.parse(respuesta);
+        let errorAjax = document.querySelector("#error-ajax");
+        if (xhr.status == 200) {
+            errorAjax.classList.add("invisible");
+            let respuesta = xhr.responseText;
+            //JON.parsear = convertir a valor json
+            let pacientes = JSON.parse(respuesta);
+            pacientes.forEach(function (paciente) {
+                adicionarPaciente(paciente);
+            });
+        }else{
+            errorAjax.classList.remove("invisible");
+            console.log(xhr.status);
+            //.responseText = Retorna DOMException "InvalidStateError" si el tipo de respuesta no es una cadena vacía o "texto".
+            console.log(xhr.responseText);
+        };
 
-        pacientes.forEach(function (paciente) {
-            adicionarPaciente(paciente);
-            console.log(paciente);
-        });
     });
     xhr.send();
 });
