9f35562d89e93a95d22ef8d120c2188ae5931ab6
BrandoAngulo <steven.sm.1992@gmai.com>
2022-07-14 15:01:56 -0500

    curso de data science de oracle

---
+++ curso-Python_DataScience-en-OCI/1.variables-funcionesAnd-lenguajesDe-altoNivel/variables-Funciones.py
@@ -0,0 +1,6 @@
+def nombre_completo():
+   primer_nombre = input('¿Cuál es tu primer nombre? ')
+   apellido = input('¿Cuál es tu apellido? ')
+   nombre_completo = primer_nombre + ' ' + apellido
+   print(nombre_completo)
+nombre_completo()
